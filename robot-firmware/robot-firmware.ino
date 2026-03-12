2b4d67a198528ca58b0150c62df37373ccda8551
jinw0o0 <rlawlsdn4665@gmail.com>
2026-03-12 16:17:52 +0900

    test: 로그 태스트를 위해 수정

--- robot-firmware/robot-firmware.ino
+++ robot-firmware/robot-firmware.ino
@@ -15,7 +15,7 @@
 // Wi-Fi / 서버 설정 (수동 수정)
 #define WIFI_SSID     "addinedu_201class_4-2.4G"
 #define WIFI_PASSWORD "201class4!"
-#define SERVER_IP     "192.168.0.17"   // control-server IP (로컬: 127.0.0.1)
+#define SERVER_IP     "192.168.0.132"   // control-server IP (로컬: 127.0.0.1)
 
 #define SERVER_TCP_PORT 8000           // control-server AGV TCP 포트
 #define SERVER_UDP_PORT 7070
